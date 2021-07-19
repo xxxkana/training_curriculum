654b9de5bb9669bb4334728428b091efbd28f6ff
xxxkana <kana_m.0421@icloud.com>
2021-07-19 15:46:03 +0900

    修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -26,7 +26,7 @@ class CalendarsController < ApplicationController
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
-
+    
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
